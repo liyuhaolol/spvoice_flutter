6d05c48f9a26ff2808a42efc174c7c38ca66aba7
liyuhao <liyuhaoid@sina.com>
2022-08-29 17:11:45 +0800

    修改访问链接

--- lib/network/httpconstants.dart
+++ lib/network/httpconstants.dart
@@ -1,17 +1,17 @@
 class HttpConstants{
-  static const String HOST = 'https://gitee.com';
+  static const String HOST = 'https://raw.fastgit.org';
 
   ///得到栏目
-  static const String GET_TAB = HOST + '/TheCoders/ntzs/raw/master/channelList.json';
+  static const String GET_TAB = HOST + '/liyuhaolol/spvoice_flutter/master/channelList.json';
 
   ///范例而已
   static const int PULL_DOWN = 1;
   static const int PULL_UP = 2;
   ///得到新闻列表下拉
-  static const String GET_NEWS_LIST_DOWN = HOST + '/TheCoders/ntzs/raw/master/firstPage.json';
+  static const String GET_NEWS_LIST_DOWN = HOST + '/liyuhaolol/spvoice_flutter/master/firstPage.json';
 
   ///得到新闻列表上拉
-  static const String GET_NEWS_LIST_UP = HOST + '/TheCoders/ntzs/raw/master/secondPage.json';
+  static const String GET_NEWS_LIST_UP = HOST + '/liyuhaolol/spvoice_flutter/master/secondPage.json';
 
   ///只是个例子而已
   static String getPullUrl(int pullType){
